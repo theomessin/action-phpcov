6f6606b2b06e75bb8baa712c9283775296d22f89
Theodore Messinezis <theodore@messinezis.com>
2020-01-29 02:01:49 +0000

    Return report url as output

--- src/deploy.ts
+++ src/deploy.ts
@@ -1,7 +1,7 @@
 import { createDeployment } from "now-client";
 
-export default async function (token: string, path: string) {
-    const events = createDeployment({token: token, path: path}, {})
+export default async function (name: string, token: string, path: string) {
+    const events = createDeployment({token: token, path: path}, {name: name})
     for await (const event of events)
       if (event.type === 'ready')
         return event.payload;
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import purdy from "purdy";
+import * as temp from "tmp";
 import deploy from "./deploy";
 import {exec} from "@actions/exec";
 import * as core from "@actions/core";
@@ -5,22 +7,34 @@ import * as github from "@actions/github";
 import * as webhooks from "@octokit/webhooks";
 
 (async function (): Promise<void> {
+    // Temporary directory to store our reports.
+    const temporary = temp.dirSync().name;
+
     // Retrieve action inputs using actions core sdk.
     const token = core.getInput("now_token");
 
-    // Prepare reference to use to publish coverage.
-    const head: string = github.context.payload.after;
-    const reference = `phpcov-${head.substr(0, 16)}`;
+    // Prepare report paths.
+    const coverage_html = `${temporary}/html_report`;
+    const coverage_xml = `${temporary}/report.xml`;
 
     // Prepare arguments for phpunit spawn.
     const cmd = "./vendor/bin/phpunit";
-    const args = [`--coverage-html=/tmp/${reference}`];
+    const args = [
+        `--coverage-html=${coverage_html}`,
+        `--coverage-xml=${coverage_xml}`,
+    ];
     const opts = {cwd: process.env.GITHUB_WORKSPACE};
 
     // Run PHPUnit to produce an html code coverage report.
-    await exec(cmd, args, opts)
+    await exec(cmd, args, opts);
+
+    // Prepare github event context variables.
+    const commit: string = github.context.payload.after;
+
+    // Prepare domain to use to publish coverage.
+    const domain = `phpcov-${commit.substr(0, 7)}`;
 
     // Deploy coverage report using Now.
-    const deployment = deploy(token, `/tmp/${reference}`);
-    console.log(await deployment);
+    const deployment = await deploy(domain, token, coverage_html);
+    core.setOutput("report_url", deployment.url);
 })();
