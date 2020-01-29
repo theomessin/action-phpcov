c2430fce5a467f39907a3794a15a3e9b9bb29556
Theodore Messinezis <theodore@messinezis.com>
2020-01-29 18:13:46 +0000

    Let's try this again.

--- src/clover.ts
+++ src/clover.ts
@@ -1,71 +1,71 @@
+export interface Line {}
+
+export interface ClassMetricsAttributes {
+    complexity: number;
+    elements: number;
+    coveredelements: number;
+    conditionals: number;
+    coveredconditionals: number;
+    statements: number;
+    coveredstatements: number;
+    coveredmethods: number;
+    methods: number;
+    testduration: number;
+    testfailures: number;
+    testpasses: number;
+    testruns: number;
+}
+
+export interface FileMetricsAttributes extends ClassMetricsAttributes {
+    classes: number;
+    loc: number;
+    ncloc: number;
+}
+
+export interface PackageMetricsAttributes extends FileMetricsAttributes {
+    files: number;
+}
+
+export interface ProjectMetricsAttributes extends PackageMetricsAttributes {
+    packages: number;
+}
+
 export interface ClassMetrics {
-    "@complexity": number;
-    "@elements": number;
-    "@coveredelements": number;
-    "@conditionals": number;
-    "@coveredconditionals": number;
-    "@statements": number;
-    "@coveredstatements": number;
-    "@coveredmethods": number;
-    "@methods": number;
-    "@testduration": number;
-    "@testfailures": number;
-    "@testpasses": number;
-    "@testruns": number;
+    "@": ClassMetricsAttributes;
 }
 
-export interface FileMetrics extends ClassMetrics {
-    "@classes": number;
-    "@loc": number;
-    "@ncloc": number;
+export interface FileMetrics {
+    "@": FileMetricsAttributes;
 }
 
-export interface PackageMetrics extends FileMetrics {
-    "@files": number;
+export interface PackageMetrics {
+    "@": PackageMetricsAttributes;
 }
 
-export interface ProjectMetric extends PackageMetrics{
-    "@packages": number;
-}
-
-export interface Line {
-    "@num": number;
-    "@type": string;
-    "@complexity": number;
-    "@count": number;
-    "@falsecount": number;
-    "@truecount": number;
-    "@signature": string;
-    "@testduration": number;
-    "@testsuccess": boolean;
-    "@visibility": string;
+export interface ProjectMetrics {
+    "@": ProjectMetricsAttributes;
 }
 
 export interface Class {
-    "@name": string;
-    metric: ClassMetrics[];
+    metrics: ClassMetrics[];
 }
 
 export interface File {
-    "@name": string;
     class: Class[];
     line: Line[];
-    metric: FileMetrics[];
+    metrics: FileMetrics[];
 }
 
 export interface Package {
-    "@name": string;
     file: File[];
 }
 
 export interface Project {
-    "@timestamp": number;
-    metric: ProjectMetric[];
+    metrics: ProjectMetrics[];
     package: Package[];
 }
 
 export interface Coverage {
-    "@generated": number;
     project: Project[];
 }
 
--- src/index.ts
+++ src/index.ts
@@ -39,8 +39,7 @@ import * as github from "@actions/github";
 
     // Parse coverage metrics from clover xml report.
     const clover: clover.Root = await parse(clover_path);
-    console.log(clover.coverage.project);
-    // const percentage = metrix["@coveredstatements"] / metrix["@statements"];
-    // console.log(metrix["@coveredstatements"], metrix["@statements"]);
-    // console.log(`Coverage percentage is ${percentage}`);
+    const metrix = clover.coverage.project[0].metrics[0]["@"];
+    const percentage = metrix.coveredstatements / metrix.statements;
+    console.log(metrix, `Coverage percentage is ${percentage}`);
 })();
